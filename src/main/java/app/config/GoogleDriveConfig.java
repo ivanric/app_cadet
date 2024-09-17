b888d02b6e9fb0a15e76b4068197d66706006019
ivan mamani <ivanric12@gmail.com>
2024-09-17 10:52:03 -0400

    commit:1.5.6

--- src/main/java/app/config/GoogleDriveConfig.java
+++ src/main/java/app/config/GoogleDriveConfig.java
@@ -1,3 +1,66 @@
+//package app.config;
+//
+//import com.google.api.client.auth.oauth2.Credential;
+//import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+//import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+//import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+//import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+//import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+//import com.google.api.client.http.javanet.NetHttpTransport;
+//import com.google.api.client.json.JsonFactory;
+//import com.google.api.client.json.jackson2.JacksonFactory;
+//import com.google.api.client.util.store.FileDataStoreFactory;
+//import com.google.api.services.drive.Drive;
+//import com.google.api.services.drive.DriveScopes;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.security.GeneralSecurityException;
+//import java.util.Collections;
+//import java.util.List;
+//
+//import org.springframework.context.annotation.Configuration;
+//@Configuration
+//public class GoogleDriveConfig {
+//	//config
+//    private static final String APPLICATION_NAME = "Google Drive API Java";
+//    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+//    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+//   
+//    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
+//    private static final String CREDENTIALS_FILE_PATH = "/appwebdriver.json";  // Debe coincidir con el nombre del archivo en resources
+////  private static final String CREDENTIALS_FILE_PATH = "/drivelocal.json";  // Debe coincidir con el nombre del archivo en resources
+//
+//    public static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+//        // Cargar las credenciales desde el classpath
+//        InputStream inputStream = GoogleDriveConfig.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+//        if (inputStream == null) {
+//            throw new IOException("Resource not found: " + CREDENTIALS_FILE_PATH);
+//        }
+//        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+//
+//        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, inputStreamReader);
+//
+//        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+//                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+//                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+//                .setAccessType("offline")
+//                .build();
+////        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+//        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8080).build();
+//        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+//    }
+//
+//    public static Drive getDriveService() throws IOException, GeneralSecurityException {
+//        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+//        return new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+//                .setApplicationName(APPLICATION_NAME)
+//                .build();
+//    }
+//}
+
+
 package app.config;
 
 import com.google.api.client.auth.oauth2.Credential;
@@ -21,16 +84,16 @@ import java.util.Collections;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class GoogleDriveConfig {
-	//config
+    // Configuración
     private static final String APPLICATION_NAME = "Google Drive API Java";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
    
     private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
     private static final String CREDENTIALS_FILE_PATH = "/appwebdriver.json";  // Debe coincidir con el nombre del archivo en resources
-//  private static final String CREDENTIALS_FILE_PATH = "/drivelocal.json";  // Debe coincidir con el nombre del archivo en resources
 
     public static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Cargar las credenciales desde el classpath
@@ -47,7 +110,8 @@ public class GoogleDriveConfig {
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-//        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+
+        // Configura el receptor para el entorno de producción
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8080).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
@@ -59,3 +123,4 @@ public class GoogleDriveConfig {
                 .build();
     }
 }
+
